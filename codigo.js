17331fa3b7294778c6c5a90cd9b3fea5bc9e2512
Toya <toya.dapena@xilon.es>
2022-07-26 08:47:41 +0200

    código sorteo

--- codigo.js
+++ codigo.js
@@ -1,13 +1,35 @@
 //Algoritmo para hacer la lista de la compra
 
 
-let producto = prompt("Ingresa un producto para la lista de la compra (ESC para salir)");
-let listaCompra = "";
+// let producto = prompt("Ingresa un producto para la lista de la compra (ESC para salir)");
+// let listaCompra = "";
 
-while (producto != "ESC") {
-    listaCompra = listaCompra + producto + "\n";
-    producto = prompt("Ingresa un producto para la lista de la compra");
+// while (producto != "ESC") {
+//     listaCompra = listaCompra + producto + "\n";
+//     producto = prompt("Ingresa un producto para la lista de la compra");
 
+// }
+// console.log("Lista de la compra: \n" + listaCompra);
+
+
+let numeroPremiado = parseInt(prompt("Sorteo!! Ingresar un número del 1 al 10"));
+
+switch(numeroPremiado){
+  case 1:
+  case 2:  
+  case 3:  
+  case 4:  
+  case 5:  
+  case 6:
+  case 7:
+  case 8:
+  case 10:
+    alert("Lo siento, no has acertado!!");
+    break;
+  case 9:
+    alert("PREMIO! Has acertado");
+    break;
+  default:
+    alert("Lo siento, este número está fuera del rango");
+    break;
 }
-console.log("Lista de la compra: \n" + listaCompra);
-
