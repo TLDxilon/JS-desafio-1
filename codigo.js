2644999e5909d3ca35ccf35bd318bdb5801d09c2
Toya <toya.dapena@xilon.es>
2022-07-26 22:55:29 +0200

    utilizo estructura while

--- codigo.js
+++ codigo.js
@@ -1,15 +1,3 @@
-// let numero = parseInt(prompt("Sorteo!! Ingresar un número del 1 al 10"));
-
-
-// while(numero != 9){
-//   alert("Lo siento, no has acertado");
-//   numero = parseInt(prompt("Sorteo!! Ingresar un número del 1 al 10. Tienes 3 oportunidades"));
-// }
-
-//   alert("Enhorabuena! Has acertado!")
-
-
-
 let numero = parseInt(prompt("Sorteo!! Ingresar un número del 1 al 10"));
 if(numero=="9"){
     alert("Enhorabuena! Has acertado!");
